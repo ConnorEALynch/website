d4395b1dae8710e2aefbab499e6e620a06bb414f
unknown <empirepenguins@gmail.com>
2023-09-07 19:58:26 -0400

    health check is an issue forcing a success with AWS certs. will sove thuroghly later

--- config/lego-script.sh
+++ config/lego-script.sh
@@ -1,1 +1,1 @@
-lego --accept-tos --email="connorealynch@gmail.com" -d "connorlynch.ca" -d "www.connorlynch.ca" -d "collection.connorlynch.ca" -d "tes.connorlynch.ca" --dns lightsail $MODE
+lego --accept-tos --email="connorealynch@gmail.com" -d "connorlynch.ca" -d "www.connorlynch.ca" -d "collection.connorlynch.ca" -d "meh.connorlynch.ca" --dns lightsail $MODE
